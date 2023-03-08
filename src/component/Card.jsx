06394c7d375d15eb440fc84901a3077abbb91e10
cyranorock <ciranorock@gmail.com>
2023-03-08 20:03:04 -0300

    add style

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
-import styles from './Card.module.scss'
+import styles from './Card.module.css'
 
 function Card({ userName , banda }) {
   return (
-    <div>Hola {userName} tu banda favorito es {banda}</div>
+    <div className={styles.elem}>Hola {userName} tu banda favorita es: {banda}</div>
   )
 }
 export default Card
